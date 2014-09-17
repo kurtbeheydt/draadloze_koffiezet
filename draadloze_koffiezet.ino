1dda54606fc2a9a791f1862c4cca3b902172b6a3
Kurt Beheydt <kurt@postduif.be>
2014-09-17 11:28:48 +0200

    - zet aan functie toegevoegd - servo's vervangen door relays

--- draadloze_koffiezet.ino
+++ draadloze_koffiezet.ino
@@ -1,17 +1,18 @@
 // draadloze koffiezet
-
-#include <Servo.h>
 #include <YunClient.h>
 #include <YunServer.h>
 
 int ledGroen = 3;
-int servoPin = 9;
-Servo servoKnop;
+int pinAanRelay = 4;
+int pinKoffieRelay = 5;
+
 YunServer server;
 
 void setup() {
-  // mode juist zetten voor de statusleds
+  // mode juist zetten voor de statusled en relays
   pinMode(ledGroen, OUTPUT);
+  pinMode(pinAanRelay, OUTPUT);
+  pinMode(pinKoffieRelay, OUTPUT);
   
   // Laden van Yun bridge library.
   Bridge.begin();
@@ -23,10 +24,6 @@ void setup() {
   
   // status: communicatie = ok
   digitalWrite(ledGroen, HIGH);
-  
-  // servo koppelen en naar beginstand zetten
-  servoKnop.attach(servoPin);
-  servoKnop.write(0); 
 } 
  
 void loop() {
@@ -43,11 +40,12 @@ void loop() {
     command.trim();
     
     // als het commando 'koffie' binnenkomt, de bezochte webpagina is dus http://koffiezet.local/arduino/koffie
-    if (command == "koffie") {
-      servoKnop.write(180); 
-    } else if (command == "reset") { // reset mogelijkheid om servo terug naar begin positie te zetten, de bezochte webpagina is dus http://koffiezet.local/arduino/reset
-      servoKnop.write(0); 
-    }
+    if (command == "zetaan") { // om machine aan te zetten, de bezochte webpagina is dus http://koffiezet.local/arduino/zetaan
+      digitalWrite(pinAanRelay, HIGH);
+      delay(500); // aan-knop van senseo heeft wat indruktijd nodig
+    } else if (command == "koffie") {
+      digitalWrite(pinKoffieRelay, HIGH);
+    }  
 
     // antwoord met de standaardpagina met de knop om koffie te maken, voor eender welke bezochte pagina onder http://koffiezet.local/arduino/
     client.println("Status: 200");
@@ -55,9 +53,14 @@ void loop() {
     client.println();
     client.print("<!DOCTYPE HTML><html lang=\"nl\" dir=\"ltr\"><head><title>zet koffie!</title></head>");
     client.print("<body style=\"text-align: center; padding-top: 200px; font-family: Helvetica; font-size: 120px; background-color: #dddddd;\">");
-    client.print("<a href=\"/arduino/koffie\" style=\"display: block; padding: 50px 20px; background-color: #333333; color: #ffffff; width: 80%; margin: auto;");
+    client.print("<a href=\"/arduino/zetaan\" style=\"display: block; padding: 50px 20px; background-color: #111111; color: #ffffff; width: 80%; margin: 20px auto;");
+    client.print(" text-decoration: none; -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;\">zet aan!</a>");
+    client.print("<a href=\"/arduino/koffie\" style=\"display: block; padding: 50px 20px; background-color: #333333; color: #ffffff; width: 80%; margin: 20px auto;");
     client.print(" text-decoration: none; -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;\">zet koffie!</a>");
     client.println("</body></html>");
     client.stop();
+  } else {
+     digitalWrite(pinKoffieRelay, LOW);
+     digitalWrite(pinAanRelay, LOW);
   }
 }
